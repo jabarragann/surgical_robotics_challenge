0e3f886d4df334c7ed0bba9c7bcc819a7760c0c8
Juan Antonio Barragan <jbarrag3@jh.edu>
2021-12-04 01:27:10 -0500

    estimate ellipse finished

--- juan-scripts/estimate_ellipse_3D_pose.py
+++ juan-scripts/estimate_ellipse_3D_pose.py
@@ -1,45 +1,13 @@
-from os import W_OK
-import pickle
 import numpy as np
 import pandas as pd
-import math as m
 import cv2
-from numpy.linalg import norm
-from numpy import cos, sin, pi
-from cv_bridge import CvBridge, CvBridgeError
-import rospy
-from sensor_msgs.msg import Image
-from numpy.linalg import inv
-from surgical_robotics_challenge.scene import Scene
-from surgical_robotics_challenge.camera import Camera
 from ambf_client import Client
 import time
-import tf_conversions.posemath as pm
 from autonomy_utils.circle_pose_estimator import Circle3D, Ellipse2D, CirclePoseEstimator
+from autonomy_utils.ambf_utils import AMBFCameras, ImageSaver, AMBFNeedle
+import rospy
 
 np.set_printoptions(precision=6)
-
-
-class ImageSaver:
-    def __init__(self):
-        self.bridge = CvBridge()
-        self.img_subs = rospy.Subscriber(
-            "/ambf/env/cameras/cameraL/ImageData", Image, self.left_callback
-        )
-
-        self.left_frame = None
-        self.left_ts = None
-
-        # Wait a until subscribers and publishers are ready
-        rospy.sleep(0.5)
-
-    def left_callback(self, msg):
-        try:
-            cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            self.left_frame = cv2_img
-            self.left_ts = msg.header.stamp
-        except CvBridgeError as e:
-            print(e)
 
 
 if __name__ == "__main__":
@@ -50,59 +18,29 @@ if __name__ == "__main__":
     c = Client("juanclient")
     c.connect()
     time.sleep(0.3)
-
-    scene = Scene(c)
-    ambf_cam_l = Camera(c, "cameraL")
-    ambf_cam_frame = Camera(c, "CameraFrame")
-    # Ground truth
-    radius = 0.1018
-
-    # Read camera parameters
-    fvg = 1.2
-    width = 640
-    height = 480
-    f = height / (2 * np.tan(fvg / 2))
-
-    intrinsic_params = np.zeros((3, 3))
-    intrinsic_params[0, 0] = f
-    intrinsic_params[1, 1] = f
-    intrinsic_params[0, 2] = width / 2
-    intrinsic_params[1, 2] = height / 2
-    intrinsic_params[2, 2] = 1.0
-    mtx = intrinsic_params
-
-    focal_length = (mtx[0, 0] + mtx[1, 1]) / 2
+    needle_handle = AMBFNeedle(ambf_client=c)
+    camera_handle = AMBFCameras()
 
     # Get 3D position of the tip and tail
-    theta = np.array([np.pi / 3, np.pi]).reshape((2, 1))
-    radius = 0.1018
-    needle_salient = radius * np.hstack(
-        (np.cos(theta), np.sin(theta), theta * 0, np.ones((2, 1)) / radius)
-    )
+    needle_salient = needle_handle.get_tip_tail_pose()
 
-    T_WN = pm.toMatrix(scene.needle_measured_cp())  # Needle to world
-    T_FC = pm.toMatrix(ambf_cam_l.get_T_c_w())  # CamL to CamFrame
-    T_WF = pm.toMatrix(ambf_cam_frame.get_T_c_w())  # CamFrame to world
-
-    T_WC = T_WF.dot(T_FC)
-    T_CN = inv(T_WC).dot(T_WN)
-
-    # Convert AMBF camera axis to Opencv Camera axis
-    F = np.array([[0, 1, 0, 0], [0, 0, -1, 0], [-1, 0, 0, 0], [0, 0, 0, 1]])
-    T_CN = F @ T_CN
+    # Get needle pose wrt camera
+    T_CN = needle_handle.get_current_pose()
 
     tip_tail_pt = T_CN @ needle_salient.T
     plane_vect = tip_tail_pt[:3, 0] - tip_tail_pt[:3, 1]
 
     # Normalize ellipse coefficients
     ellipse = Ellipse2D.from_coefficients("./juan-scripts/output/ellipse_coefficients.txt")
-    estimator = CirclePoseEstimator(ellipse, mtx, focal_length, radius)
+    estimator = CirclePoseEstimator(
+        ellipse, camera_handle.mtx, camera_handle.focal_length, needle_handle.radius
+    )
     circles = estimator.estimate_pose()
 
     print("algorithm summary")
     print("camera_matrix")
-    print(mtx)
-    print("focal length {:0.4f}".format(focal_length))
+    print(camera_handle.mtx)
+    print("focal length {:0.4f}".format(camera_handle.focal_length))
     print("ellipse c matrix")
     print(estimator.c_mat)
     # print("eigen values")
@@ -131,10 +69,10 @@ if __name__ == "__main__":
         # Sample 3D circle
         pts = circles[i].generate_parametric(30)
         df = pd.DataFrame(pts.T, columns=["x", "y", "z"])
-        df.to_csv("circle{:d}.txt".format(i), index=None)
+        df.to_csv("./juan-scripts/output/circle{:d}.txt".format(i), index=None)
 
         # Draw 3D circle
-        img = circles[i].project_pt_to_img(img, mtx, 30)
+        img = circles[i].project_pt_to_img(img, camera_handle.mtx, 30)
 
         # #Draw ellipse samples
         for xp, yp in zip(X.squeeze(), Y.squeeze()):
---
+++ solution-scripts/autonomy_utils/ambf_utils.py
@@ -0,0 +1,103 @@
+from cv_bridge import CvBridge, CvBridgeError
+import rospy
+from sensor_msgs.msg import Image
+import time
+from surgical_robotics_challenge.scene import Scene
+from surgical_robotics_challenge.camera import Camera
+
+import tf_conversions.posemath as pm
+import numpy as np
+from numpy.linalg import inv, norm
+
+
+class ImageSaver:
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.img_subs = rospy.Subscriber(
+            "/ambf/env/cameras/cameraL/ImageData", Image, self.left_callback
+        )
+
+        self.left_frame = None
+        self.left_ts = None
+
+        # Wait a until subscribers and publishers are ready
+        rospy.sleep(0.5)
+
+    def left_callback(self, msg):
+        try:
+            cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.left_frame = cv2_img
+            self.left_ts = msg.header.stamp
+        except CvBridgeError as e:
+            print(e)
+
+
+class AMBFNeedle:
+    def __init__(self, ambf_client) -> None:
+
+        self.c = ambf_client
+        self.scene = Scene(self.c)
+        self.ambf_cam_l = Camera(self.c, "cameraL")
+        self.ambf_cam_frame = Camera(self.c, "CameraFrame")
+
+        self.radius = 0.1018
+
+    def get_tip_tail_pose(self) -> np.ndarray:
+        """Get 3D positions of the tip and the tail w.r.t the needle local frame.
+        The needle is parametrized by a circle of radius `radius`. The tip of the needle is
+        located at the angle pi/3 and the tail at  pi.
+
+        Returns:
+            np.ndarray: [description]
+        """
+        # Get 3D position of the tip and tail
+        theta = np.array([np.pi / 3, np.pi]).reshape((2, 1))
+        radius = 0.1018
+        needle_salient = radius * np.hstack(
+            (np.cos(theta), np.sin(theta), theta * 0, np.ones((2, 1)) / radius)
+        )
+        return needle_salient
+
+    def get_current_pose(self) -> np.ndarray:
+        """Generates the needle current pose with respect to the left camera frame. The resulting matrix
+            uses the opencv convention instead of the AMBF convention.
+
+        Returns:
+            np.ndarray: 4x4 transformation matrix representing the needle pose with respect to the camera
+
+        """
+
+        T_WN = pm.toMatrix(self.scene.needle_measured_cp())  # Needle to world
+        T_FC = pm.toMatrix(self.ambf_cam_l.get_T_c_w())  # CamL to CamFrame
+        T_WF = pm.toMatrix(self.ambf_cam_frame.get_T_c_w())  # CamFrame to world
+
+        T_WC = T_WF @ T_FC
+        T_CN = inv(T_WC) @ T_WN
+
+        # Convert AMBF camera axis to Opencv Camera axis
+        F = np.array([[0, 1, 0, 0], [0, 0, -1, 0], [-1, 0, 0, 0], [0, 0, 0, 1]])
+        T_CN = F @ T_CN
+
+        return T_CN
+
+    def sample_3d_pts():
+        pass
+
+
+class AMBFCameras:
+    def __init__(self) -> None:
+        # compute camera intrinsics
+        self.fvg = 1.2
+        self.width = 640
+        self.height = 480
+        self.f = self.height / (2 * np.tan(self.fvg / 2))
+
+        intrinsic_params = np.zeros((3, 3))
+        intrinsic_params[0, 0] = self.f
+        intrinsic_params[1, 1] = self.f
+        intrinsic_params[0, 2] = self.width / 2
+        intrinsic_params[1, 2] = self.height / 2
+        intrinsic_params[2, 2] = 1.0
+        self.mtx = intrinsic_params
+
+        self.focal_length = (self.mtx[0, 0] + self.mtx[1, 1]) / 2

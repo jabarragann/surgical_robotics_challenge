4973856a75d7c68cfccdf28f1c9c0833c54c4c53
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-01-25 17:02:36 -0500

    clean collect dataset script

--- juan-scripts/examples/collect_segmentation_dataset.py
+++ juan-scripts/examples/collect_segmentation_dataset.py
@@ -14,44 +14,6 @@ import datetime
 np.set_printoptions(precision=3, suppress=True)
 
 
-def clean_image(frame, camera_selector):
-    # Paint needle points
-    needle_handle = AMBFNeedle(ambf_client=c, logger=log)
-    camera_handle = AMBFCamera(ambf_client=c, camera_selector=camera_selector)
-    stereo_rig_handle = AMBFStereoRig(ambf_client=c)
-    needle_pts = needle_handle.sample_3d_pts(10)
-    T_CN = needle_handle.get_needle_to_camera_pose(camera_selector)
-    img_pt = camera_handle.project_points(T_CN, needle_pts)
-
-    # Display image
-    min_x, max_x = 2000, 0
-    min_y, max_y = 2000, 0
-    for i in range(img_pt.shape[0]):
-        if img_pt[i, 0, 0] < min_x:
-            min_x = img_pt[i, 0, 0]
-        if img_pt[i, 0, 0] > max_x:
-            max_x = img_pt[i, 0, 0]
-        if img_pt[i, 0, 1] < min_y:
-            min_y = img_pt[i, 0, 1]
-        if img_pt[i, 0, 1] > max_y:
-            max_y = img_pt[i, 0, 1]
-        # frame = cv2.circle(frame, (int(img_pt[i, 0, 0]), int(img_pt[i, 0, 1])), 5, (255, 0, 0), -1)
-
-    mask = np.zeros((1080, 1920), dtype="uint8")
-    shift = 60
-    mask = cv2.rectangle(
-        mask,
-        (int(min_x - shift), int(min_y - shift)),
-        (int(max_x + shift), int(max_y + shift)),
-        (255, 0, 0),
-        -1,
-    )
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_and(gray, mask)
-    frame = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-    return frame
-
-
 def save_image(path, l_img, l_seg, r_img, r_seg):
     # generate timestamp
     ts = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -67,7 +29,7 @@ def main():
     w_name = "disp_final"
     cv2.namedWindow(w_name, cv2.WINDOW_NORMAL)
 
-    p = Path("/home/juan1995/research_juan/needle_dataset_/d3")
+    p = Path("/home/juan1995/research_juan/needle_dataset_/d4")
     if not p.exists():
         p.mkdir(parents=True)
 
@@ -91,8 +53,12 @@ def main():
         if k == ord("q"):
             break
         elif k == ord("s"):
-            clean_seg_l = clean_image(segmented_l, "left")
-            clean_seg_r = clean_image(segmented_r, "right")
+            clean_seg_l = image_segmentation.clean_image(
+                segmented_l, "left", ambf_client=c, log=log
+            )
+            clean_seg_r = image_segmentation.clean_image(
+                segmented_r, "right", ambf_client=c, log=log
+            )
 
             final_2 = np.hstack((clean_seg_l, clean_seg_r))
             save_image(p, frame_l, clean_seg_l, frame_r, clean_seg_r)
--- solution-scripts/autonomy_utils/image_segmentation.py
+++ solution-scripts/autonomy_utils/image_segmentation.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from autonomy_utils.ambf_utils import AMBFStereoRig, AMBFCamera, AMBFNeedle
 
 
 def segment_needle(frame):
@@ -22,3 +23,52 @@ def segment_needle(frame):
     img_erode = cv.cvtColor(img_erode, cv.COLOR_GRAY2BGR)
 
     return img_erode
+
+
+def clean_image(frame: np.ndarray, camera_selector: str, ambf_client=None, log=None) -> np.ndarray:
+    """Clean segmentation mask using the location of the needle w.r.t world coordinates.
+
+    Args:
+        frame (np.ndarray): [description]
+        camera_selector (str): [description]
+        ambf_client ([type], optional): [description]. Defaults to None.
+        log ([type], optional): [description]. Defaults to None.
+
+    Returns:
+        np.ndarray: Clean image
+    """
+    # Paint needle points
+    needle_handle = AMBFNeedle(ambf_client=ambf_client, logger=log)
+    camera_handle = AMBFCamera(ambf_client=ambf_client, camera_selector=camera_selector)
+    stereo_rig_handle = AMBFStereoRig(ambf_client=ambf_client)
+    needle_pts = needle_handle.sample_3d_pts(10)
+    T_CN = needle_handle.get_needle_to_camera_pose(camera_selector)
+    img_pt = camera_handle.project_points(T_CN, needle_pts)
+
+    # Display image
+    min_x, max_x = 2000, 0
+    min_y, max_y = 2000, 0
+    for i in range(img_pt.shape[0]):
+        if img_pt[i, 0, 0] < min_x:
+            min_x = img_pt[i, 0, 0]
+        if img_pt[i, 0, 0] > max_x:
+            max_x = img_pt[i, 0, 0]
+        if img_pt[i, 0, 1] < min_y:
+            min_y = img_pt[i, 0, 1]
+        if img_pt[i, 0, 1] > max_y:
+            max_y = img_pt[i, 0, 1]
+        # frame = cv2.circle(frame, (int(img_pt[i, 0, 0]), int(img_pt[i, 0, 1])), 5, (255, 0, 0), -1)
+
+    mask = np.zeros((1080, 1920), dtype="uint8")
+    shift = 60
+    mask = cv.rectangle(
+        mask,
+        (int(min_x - shift), int(min_y - shift)),
+        (int(max_x + shift), int(max_y + shift)),
+        (255, 0, 0),
+        -1,
+    )
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.bitwise_and(gray, mask)
+    frame = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
+    return frame

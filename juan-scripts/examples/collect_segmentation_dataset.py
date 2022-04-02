b3e6abb61c00345eb40c1f6d1b482e1362649c98
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-04-02 19:42:54 -0400

    updated data collection script

--- juan-scripts/examples/collect_segmentation_dataset.py
+++ juan-scripts/examples/collect_segmentation_dataset.py
@@ -1,3 +1,4 @@
+import os
 from re import I
 from autonomy_utils.ambf_utils import AMBFStereoRig, ImageSaver, AMBFCamera, AMBFNeedle
 from autonomy_utils import Logger
@@ -24,23 +25,41 @@ def save_image(path, l_img, l_seg, r_img, r_seg):
     cv2.imwrite(str(path / f"{ts}_r_seg.jpeg"), r_seg)
 
 
+def save_image(path: Path, image: np.ndarray, mask: np.ndarray, next_image: int):
+    final_path = path / f"{next_image:04d}"
+
+    if not (final_path / "images").exists():
+        (final_path / "images").mkdir(parents=True)
+    if not (final_path / "masks").exists():
+        (final_path / "masks").mkdir(parents=True)
+
+    cv2.imwrite(str((final_path / "images") / f"{next_image:04d}_image.jpeg"), image)
+    cv2.imwrite(str((final_path / "masks") / f"{next_image:04d}_mask.jpeg"), mask)
+
+
 def main():
     image_subs = ImageSaver()
     w_name = "disp_final"
     cv2.namedWindow(w_name, cv2.WINDOW_NORMAL)
 
-    print(f"Data collection util cmds")
-    print(f"Press 's' to save new images")
-    print(f"Press 'q' to quit")
-
-    p = Path("/home/juan1995/research_juan/needle_dataset_/d6")
+    p = Path("/home/juan1995/research_juan/needle_dataset_/d8")
     if not p.exists():
         p.mkdir(parents=True)
+
+    # Calculate the next number
+    sub_dirs = [int(x.name) for x in p.glob("*")]
+    next_img = max(sub_dirs) + 1
 
     needle_seg = NeedleSegmenter(ambf_client=c, log=log)
     camera_selector = "left"
 
     count = 0
+
+    print(f"Data collection util cmds")
+    print(f"Save next image to {next_img:04d}")
+    print(f"Press 's' to save new images")
+    print(f"Press 'q' to quit")
+
     while True:
         frame_l = image_subs.get_current_frame("left")
         frame_r = image_subs.get_current_frame("right")
@@ -48,6 +67,7 @@ def main():
         segmented_r = NeedleSegmenter.segment_needle(frame_r)
         clean_seg_l = needle_seg.clean_image(segmented_l, "left", ambf_client=c, log=log)
 
+        # final = np.hstack((frame_l, segmented_l))
         final = np.hstack((frame_l, clean_seg_l))
         # log.info(f"T_CN \n {T_CN}")
         # log.info(f"T_WF \n {stereo_rig_handle.get_current_pose()}")
@@ -64,8 +84,12 @@ def main():
             clean_seg_r = needle_seg.clean_image(segmented_r, "right", ambf_client=c, log=log)
 
             # final_2 = np.hstack((clean_seg_l, clean_seg_r))
-            print(f"image {count} saved ...")
-            save_image(p, frame_l, clean_seg_l, frame_r, clean_seg_r)
+            save_image(p, frame_l, clean_seg_l, next_img)
+            next_img += 1
+            save_image(p, frame_r, clean_seg_r, next_img)
+            next_img += 1
+            print(f"images {next_img+1:04d} and  {next_img:04d} saved ...")
+            # save_image(p, frame_l, clean_seg_l, frame_r, clean_seg_r)
             count += 1
             # cv2.namedWindow("saved", cv2.WINDOW_NORMAL)
             # cv2.imshow("saved", final_2)

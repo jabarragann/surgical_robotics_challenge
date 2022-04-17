2885333a1d58a34f8d3fd2ae80ed69cb3bb7c4e8
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-04-17 18:30:37 -0400

    test showing env incompatibility.

---
+++ juan-scripts/tests/test_loading_scripts.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from PyKDL import Frame, Rotation, Vector, Twist
+
+# matplotlib 3.5.1
+"""
+My environment has a compatibility problem between PyKDL and matplotlib.
+My problem fixes by adding the matplotlib after the PyKDL. The other way around will generated an error.
+"""
+if __name__ == "__main__":
+    print("passed")
+    pass

cd6f4d140fb5814b24b5ddb15185ae157ddcdd78
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-05-13 15:36:09 -0400

    comment

--- plugins/PreprocessingShadersConfig/shaders_config.h
+++ plugins/PreprocessingShadersConfig/shaders_config.h
@@ -26,6 +26,7 @@ protected:
     afVector3d needle_diffuse;
     // afBaseObjectMap *rigid_bodies_map_ptr;
     afRigidBodyMap rigid_bodies_map;
+    // Todo: Use smart pointers to store shader materials or deallocated the materials at the end.
     map<string, cMaterial *> new_materials_map;
     ShaderConfigObjectVector shader_config_objects;
 
